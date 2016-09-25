4be43b787c56a200d1e314b1b354c9c22fe78ae2
Jeremy Thomas <bbxdesign@gmail.com>
2016-09-25 10:45:21 +0100

    Add newsletter

--- javascript/marksheet.js
+++ javascript/marksheet.js
@@ -6,6 +6,8 @@ jQuery(document).ready(function ($) {
   var $down = $('#down');
   var $overlay = $('#overlay');
   var $top = $('#top');
+  var $banner = $('#banner');
+  var $newsletter = $('#newsletter');
 
   $toggle.click( function() {
     $('html').addClass('open-menu');
@@ -18,6 +20,13 @@ jQuery(document).ready(function ($) {
   $top.click( function() {
     $('html, body').animate({
       scrollTop: 0
+    }, 500);
+  });
+
+  $banner.click( function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $newsletter.offset().top
     }, 500);
   });
 
